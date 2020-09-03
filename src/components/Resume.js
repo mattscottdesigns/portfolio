18c136e76ed7bf11dc845543d4c36cce1269d6dc
Matt Scott <mscott@cdsreg.com>
2020-09-03 11:34:57 -0400

    Generate resume based on data

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Heading from "./Heading"
+import resume from "./../constants/resume"
 import {
   VerticalTimeline,
   VerticalTimelineElement,
@@ -13,73 +14,17 @@ class Resume extends React.Component {
         <Heading text="Resume" />
         <div className="px-10 py-16">
           <VerticalTimeline>
-            <VerticalTimelineElement date="2011 - present" className="TEST">
-              <h3 className="vertical-timeline-element-title">
-                Creative Director
-              </h3>
-              <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
-              <p>
-                Creative Direction, User Experience, Visual Design, Project
-                Management, Team Leading
-              </p>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement date="2010 - 2011">
-              <h3 className="vertical-timeline-element-title">Art Director</h3>
-              <h4 className="vertical-timeline-element-subtitle">
-                San Francisco, CA
-              </h4>
-              <p>
-                Creative Direction, User Experience, Visual Design, SEO, Online
-                Marketing
-              </p>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement date="2008 - 2010">
-              <h3 className="vertical-timeline-element-title">Web Designer</h3>
-              <h4 className="vertical-timeline-element-subtitle">
-                Los Angeles, CA
-              </h4>
-              <p>User Experience, Visual Design</p>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement
-              className="vertical-timeline-element--work"
-              date="2006 - 2008"
-            >
-              <h3 className="vertical-timeline-element-title">Web Designer</h3>
-              <h4 className="vertical-timeline-element-subtitle">
-                San Francisco, CA
-              </h4>
-              <p>User Experience, Visual Design</p>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement
-              className="vertical-timeline-element--education"
-              date="April 2013"
-            >
-              <h3 className="vertical-timeline-element-title">
-                Content Marketing for Web, Mobile and Social Media
-              </h3>
-              <h4 className="vertical-timeline-element-subtitle">
-                Online Course
-              </h4>
-              <p>Strategy, Social Media</p>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement date="November 2012">
-              <h3 className="vertical-timeline-element-title">
-                Agile Development Scrum Master
-              </h3>
-              <h4 className="vertical-timeline-element-subtitle">
-                Certification
-              </h4>
-              <p>Creative Direction, User Experience, Visual Design</p>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement date="2002 - 2006">
-              <h3 className="vertical-timeline-element-title">
-                Bachelor of Science in Interactive Digital Media Visual Imaging
-              </h3>
-              <h4 className="vertical-timeline-element-subtitle">
-                Bachelor Degree
-              </h4>
-              <p>Creative Direction, Visual Design</p>
-            </VerticalTimelineElement>
+            {resume.map(item => {
+              return (
+                <VerticalTimelineElement date={item.date} className="text-dark">
+                  <div className="text-xl font-bold">{item.title}</div>
+                  <div className="mb-5 font-thin text-primary">
+                    {item.company}
+                  </div>
+                  <div>{item.description}</div>
+                </VerticalTimelineElement>
+              )
+            })}
           </VerticalTimeline>
         </div>
       </div>
