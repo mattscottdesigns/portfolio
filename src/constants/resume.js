5e368b3fbb1beae1f1fc8a55dedd214c295f78d2
Matt Scott <mscott@cdsreg.com>
2020-09-03 11:34:20 -0400

    Create Resume data structure

---
+++ src/constants/resume.js
@@ -0,0 +1,32 @@
+export const resume = [
+  {
+    title: "Senior UI/UX Developer",
+    company: "Convention Data Services",
+    date: "November 2019 — September 2020",
+    description:
+      "Lead the direction of UI and UX decisions for our products. Worked on projects using JavaScript (ES6 / React / React Native), standardizing look and feel across products while continuously improving the UI/UX. Also lead a Committee that was in charge of evaluating and improving the next product on our company roadmap.",
+  },
+  {
+    title: "Senior Web Graphic Designer",
+    company: "Brahmin Leather Works",
+    date: "July 2019 — November 2019",
+    description:
+      "Lead web graphic design and ensured all designs met company branding. Used Adobe Creative Suite to create landing pages, digital catalogs, emails,  and advertisements. This involved working with the photography team to provide direction of the product shoot and assist with editing.",
+  },
+  {
+    title: "UI/UX Developer",
+    company: "Convention Data Services",
+    date: "July 2017 — July 2019",
+    description:
+      "Worked with product stakeholders and users to perform research of pain points and areas of opportunity for our products. This involved redesigning legacy products while also creating next generation products using Wireframes, Adobe XD, HTML/CSS, and JavaScript.",
+  },
+  {
+    title: "Web Developer",
+    company: "Convention Data Services",
+    date: "December 2015 — July 2017",
+    description:
+      "Worked with clients to setup their event database and understand their business rules to create minimal and easy to use registration website for their attendees using Classic ASP and JavaScript (jQuery). This involved working with 3rd party integrations for things like member databases.",
+  },
+]
+
+export default resume
