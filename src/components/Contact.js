7a6775db6644c93851ccd8be909cab97c2d77942
Matt Scott <mscott@cdsreg.com>
2020-09-07 14:18:36 -0400

    Create contact form components

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,31 @@
 import React from "react"
+import Heading from "./Heading"
+import ContactForm from "./ContactForm"
+import { FaGithubSquare, FaLinkedin } from "react-icons/fa"
+import { MdEmail } from "react-icons/md"
+import { FaEnvelopeSquare } from "react-icons/fa"
 
 class Contact extends React.Component {
   render() {
-    return <div id="contact">Contact</div>
+    return (
+      <div id="contact">
+        <Heading text="Contact" />
+        <div className="bg-dark">
+          <div className="flex justify-center p-3 text-4xl bg-black bg-opacity-25 text-light">
+            <a href="https://github.com/mscott087">
+              <FaGithubSquare className="mx-3" />
+            </a>
+            <a href="https://www.linkedin.com/in/matthew-taylor-scott/">
+              <FaLinkedin className="mx-3" />
+            </a>
+            <a href="mailto:matt.taylor.scott@gmail.com">
+              <FaEnvelopeSquare className="mx-3" />
+            </a>
+          </div>
+          <ContactForm />
+        </div>
+      </div>
+    )
   }
 }
 
