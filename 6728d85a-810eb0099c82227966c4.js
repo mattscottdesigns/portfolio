efbe9836b9b9468c9cb4fc16d0be58479856faba
Matt Scott <matt.taylor.scott@gmail.com>
2020-11-08 12:46:52 -0500

    Updates

--- 6728d85a-810eb0099c82227966c4.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{iNdW:function(t,n,i){"use strict";i.d(n,"a",(function(){return a}));var o=i("Lnxd");function a(t){return Object(o.a)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",stroke:"#000",strokeWidth:"2",d:"M3,3 L21,21 M3,21 L21,3"}}]})(t)}}}]);
-//# sourceMappingURL=6728d85a-810eb0099c82227966c4.js.map
