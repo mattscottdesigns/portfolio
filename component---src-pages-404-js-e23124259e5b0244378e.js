09b2244b101db917617df531535dd6a918fa2b73
Matt Scott <matt.taylor.scott@gmail.com>
2020-11-26 19:56:01 -0500

    Updates

---
+++ component---src-pages-404-js-e23124259e5b0244378e.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"7oih":function(e,t,n){"use strict";var a=n("q1tI"),u=n.n(a);t.a=function(e){var t=e.children;return u.a.createElement("div",null,t)}},w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),u=n.n(a),r=n("7oih");t.default=function(){return u.a.createElement(r.a,null,u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-e23124259e5b0244378e.js.map
